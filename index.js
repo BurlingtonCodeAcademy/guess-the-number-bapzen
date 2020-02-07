99946c0fe5f3e4a1696d6130a734f08850b722c4
bapzen <ben@thebencurve.com>
2020-02-06 22:35:00 -0500

    Commit to save work...building guess engine

--- index.js
+++ index.js
@@ -10,19 +10,47 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log("Please think of a number between 1 and 100 (inclusive).\nI will try to guess it.")
+  console.log("Hello. I am Awesome-O.\nPlease think of a number between 1 and 100 (inclusive).\nI will try to guess it.")
   let randNumResp = await ask("Have you selected a random number between 1 and 100? (Y/N)\n");
   console.log('You entered: ' + randNumResp);
 
+  //A function to generate a random integer within a specified range:
   function randomInt(max, min) {
     return Math.floor(min + (Math.random() * (max - min + 1)))
    }
 
-  let randGuess = randomInt(1, 100);
-  let firstGuess = (await ask("Is it... " + randGuess + "? (Y/N)")).toUpperCase();
+  //A function to generate an array of each number between the start and end values:
+   
+
+
+   // A function to implement binary search within a specified range
+   function guessEng(firstNum, lastNum) {
+     let left = 1;
+     let right = 99;
+     let guessArr = []
+     while (left <= right) {
+       let middle = Math.floor((left + right) / 2)
+       if 
+     }
+    if (firstNum > lastNum) {
+      return 0;
+    }
+   }
+
+  let randGuess = randomInt(1, 100);  //  <= Set the number range here
+
+  // Sanitize the input; capitalize character to pass to if...else stmt
+  let firstGuess = (await ask("Is it... " + randGuess + "? (Y/N)\n")).toUpperCase();
     if (firstGuess === 'Y') {
       console.log("Awesome-O wins again!")
       process.exit();
+    } else if (firstGuess === 'N') {
+      let secondGuess = (await ask('Hmmm...is it higher(H) or lower(L)?')).toUpperCase();
+      if (secondGuess === 'H') {
+
+      } else if (secondGuess === 'L') {
+
+      }
     }
   
   // Now try and complete the program.
